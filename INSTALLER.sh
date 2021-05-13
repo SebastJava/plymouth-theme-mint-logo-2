59829b1b099751be4631a9f5718c054a80c323ab
SebastJava <sebastjava@hotmail.ca>
2021-05-13 18:29:30 -0400

    Added color choosing in the automated INSTALLER.sh
    	modified:   INSTALLER.sh
    	modified:   mint-logo-2/mint-logo.script

--- INSTALLER.sh
+++ INSTALLER.sh
@@ -3,14 +3,29 @@
 # © 2021 Sébastien Bouchard
 # <sebastjava@hotmail.ca>
 
-echo
 echo "This installer will change your Plymouth theme, giving a new look to your boot screen."
 echo "Please enter your password for this."
+sudo mkdir -p /usr/share/plymouth/themes && sudo cp -rf mint-logo-2 /usr/share/plymouth/themes
 echo
-sudo cp -rf mint-logo-2 /usr/share/plymouth/themes/ &&
-echo &&
-echo "This will take a few seconds. Do not quit until you are invited to do so..." &&
-echo &&
+
+echo "Choose your color:"
+OPTIONS="Green Blue"
+select opt in $OPTIONS; do
+    if [ "$opt" = "Green" ]; then
+      echo "You choose Green."
+      break
+    elif [ "$opt" = "Blue" ]; then
+      echo "You choose Blue."
+      sudo sed -i 's/spinner-green.png/spinner-blue.png/' /usr/share/plymouth/themes/mint-logo-2/mint-logo.script
+      break
+    else
+      echo "Bad option. Please try again."
+    fi
+done
+
+echo
+echo "This will take a few seconds. Do not quit until you are invited to do so..."
+echo
 sudo update-alternatives --install /usr/share/plymouth/themes/default.plymouth default.plymouth /usr/share/plymouth/themes/mint-logo-2/mint-logo-2.plymouth 200 &&
 sudo update-alternatives --set default.plymouth /usr/share/plymouth/themes/mint-logo-2/mint-logo-2.plymouth &&
 sudo update-initramfs -u &&
